74e44d11469c6249b59f16858153438685b2f7cf
Kasper Bouwens <kas_bouwens@hotmail.com>
2017-11-20 15:56:04 +0100

    evaluation pipeline v1

--- evaluation.py
+++ evaluation.py
@@ -1,12 +1,29 @@
 import numpy as np
 import pandas as pd
+from costcla.metrics import savings_score
+
 
 class evaluation:
 
-    def __init__(self):
-        self.columns = ["n_samples", "recall", "precision"]
+    def __init__(self, Amounts):
+        self.columns = ["n_samples", "recall", "precision", "F1", "saving", "C_recall", "C_precision", "C_F1", "C_saving"]
         self.results = []
-        self.predictions = []
+        self.amounts = Amounts
+        self.cost_matrix = self.cost_matrix(self.amounts, .22)
+
+        self.precision = lambda tp, fp: float(tp)/(tp + fp) if (tp + fp) > 0 else 0
+        self.recall = lambda tp, fn: float(tp)/(tp + fn) if (tp + fn) > 0 else 0
+        self.F1 = lambda p, r: (2*p*r)/(p+r) if (p+r) > 0 else 0
+
+    def cost_matrix(self, Amounts, Ca):
+        cmatrix = np.ones((Amounts.shape[0], 4))
+
+        cmatrix[:, 0] *= Ca
+        cmatrix[:, 1] = Amounts
+        cmatrix[:, 2] *= Ca
+        cmatrix[:, 3] *= 0
+
+        return cmatrix
 
     def evaluate(self, y_, y, n_samples):
 
@@ -23,10 +40,50 @@ class evaluation:
             else:
                 fp += 1
 
-        recall = float(tp)/(tp+fn)
-        precision = float(tp)/(tp + fp)
-        print precision, recall
-        self.results.append([n_samples, recall, precision])
+        r = self.recall(tp, fn)
+        p = self.precision(tp, fp)
+        f1= self.F1(p,r)
+
+        savings = savings_score(np.argmax(y, axis=1), np.argmax(y_, axis=1), self.cost_matrix)
+
+        C_recall, C_precision, C_F1, C_savings = self.cost_sensitive(y_, y, self.amounts)
+
+
+        #print n_samples, r, p, f1, savings, C_recall, C_precision, C_F1, C_savings
+
+        self.results.append([n_samples, r, p, f1, savings, C_recall, C_precision, C_F1, C_savings])
+
+    def cost_sensitive(self, y_, y, Amounts):
+
+        costmatrix = np.zeros((y.shape[0], 2))
+        costmatrix[:, 0] = np.ones(y.shape[0]) * 0.22
+        costmatrix[:, 1] = Amounts
+
+        thresholds = costmatrix[:, 0] / costmatrix[:, 1]
+
+        pred = [1 if y_i > threshold else 0 for y_i, threshold in zip(y_[:, 1], thresholds)]
+        pred = np.array(pred)
+
+        tp, tn, fp, fn = 0, 0, 0, 0
+
+        for prediction, label in zip(pred, np.argmax(y, axis=1)):
+
+            if label == 1 and prediction == 1:
+                tp += 1
+            elif label == 0 and  prediction == 0:
+                tn += 1
+            elif label == 1 and prediction == 0:
+                fn += 1
+            else:
+                fp += 1
+
+        r = self.recall(tp, fn)
+        p = self.precision(tp, fp)
+        f1= self.F1(p,r)
+
+        savings = savings_score(np.argmax(y, axis=1), pred, self.cost_matrix)
+
+        return r, p, f1, savings
 
     def get_results(self):
         return pd.DataFrame(data=self.results, columns=self.columns)
--- main.py
+++ main.py
@@ -29,11 +29,13 @@ def preprocess():
 
     ids = (dataset['Card']).as_matrix()
 
+    Amounts = (dataset['Amount']).as_matrix()
+
     dataset = dataset.drop(['target', 'Card'], axis=1)
     dataset = (dataset - dataset.min()) / (dataset.max() - dataset.min())
     f = dataset.as_matrix()
 
-    return f, t, ids
+    return f, t, ids, Amounts
 
 
 def fit(x_train, y_train, n_clusters=2, epochs=1, init='random'):
@@ -96,22 +98,7 @@ def predict(theta_F, theta_T, x_test, y_test, n_samples):
 
     evaluationMLE.evaluate(predictions, y_test, n_samples)
 
-
 def mle(features, targets, alpha=0.0001):
-
-    # count positive examples and negative examples
-    pos = features[targets[:, 1] == 1, :]
-    pos_len = pos.shape[0] + alpha
-    neg_len = features.shape[0] - pos_len + alpha
-    pos = sum(pos) + alpha
-    neg = sum(features[targets[:, 0] == 1, :]) + alpha
-
-    return np.asmatrix([(neg/ neg_len), (pos / pos_len)]).transpose(), [float(neg_len) / (pos_len + neg_len), float(pos_len) / (pos_len + neg_len)]
-
-def mle2(features):
-    return np.sum(features, axis=0)/ features.shape[0]
-
-def mle3(features, targets, alpha=0.0001):
 
     # count positive examples and negative examples
     pos = features[targets[:, 1] == 1, :]
@@ -146,11 +133,34 @@ def predict_with_c(x_test, y_test, params, n_samples):
 
     evaluationEM.evaluate(f_given_x, y_test, n_samples)
 
-def sum_probas():
+def predict_c1_c2(x_test, y_test, params, n_samples):
+    p_c1, x_given_c1, p_c2, x_given_c2, x_given_f, p_f = params[0], params[1], params[2], params[3], params[4], np.array(params[5])
+
+    p_c1_c2_f = np.array([np.array(p_c_nonfraud)[:, np.newaxis]*np.array(p_c2)[:, np.newaxis].T * f for f in p_f])
+
+    p_xfraud = lambda x, c: np.prod(x*x_given_c2[:, c] + np.abs(x-1)* np.abs(x_given_c2[:, c]-1))
+
+    p_xnonfraud = lambda x, c: np.prod(x*x_given_c1[:, c] + np.abs(x-1)* np.abs(x_given_c1[:, c]-1))
+
+    p_x_given_c1_c2 = lambda x, c1, c2, f: (p_xfraud(x, c1) if f == 1 else p_xnonfraud(x, c2))
+
+    f_given_x = np.zeros((y_test.shape[0], 2))
+    for i, tx in enumerate(x_test):
+        joint = np.zeros((x_given_f.shape[1], x_given_c1.shape[1], x_given_c2.shape[1]))
+        for c1 in np.arange(x_given_c1.shape[1]):
+            for c2 in np.arange(x_given_c2.shape[1]):
+                for f in np.arange(x_given_f.shape[1]):
+                    joint[f, c1, c2] += p_x_given_c1_c2(tx, c1, c2, f) * p_c1_c2_f[f, c1, c2]
+
+        f_given_x[i, :] = (np.sum(np.sum(joint, axis=1), axis=1) / np.sum(joint))
+
+    evaluationEM2.evaluate(f_given_x, y_test, n_samples)
+
+def sum_probas(x_given_f, p_f, p_c, x_given_c):
     Nx = 2 #number of values each dimension of x can take (binary in this case)
 
-    x_given_f, p_f = np.load('pickles/p_x_given_f(fraud).npy'), np.load('pickles/p_f(fraud).npy')
-    p_c, x_given_c = np.load('pickles/p_c(nonfraud).npy'), np.load('pickles/x_given_c(nonfraud).npy')
+    #x_given_f, p_f = np.load('pickles/p_x_given_f(fraud).npy'), np.load('pickles/p_f(fraud).npy')
+    #p_c, x_given_c = np.load('pickles/p_c(nonfraud).npy'), np.load('pickles/x_given_c(nonfraud).npy')
 
     all_possible_cs = np.arange(x_given_c.shape[1])  # (Nc, )
     all_possible_fs = np.arange(x_given_f.shape[1])
@@ -177,16 +187,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', type=int, default=VERBOSE, help='0 for silent, 1 for system status, 2 for graphical mode')
     parser.add_argument('--thresh', type=int, default=THRESH, help='sets threshold for new log likelihood to make algorithm stop. 0 means no threshold')
-    parser.add_argument('--n_clusters', type=int, default=2, help='number of clusters')
+    parser.add_argument('--n_clusters', type=int, default=3, help='number of clusters')
 
     FLAGS, unparsed = parser.parse_known_args()
 
-    evaluationMLE = evaluation()
-    evaluationEM = evaluation()
-
-    X, Y, ids = preprocess()
+    X, Y, ids, Amounts = preprocess()
     ratio = int(math.ceil(.8*len(X)))
     x_train, y_train, x_test, y_test = X[:ratio], Y[:ratio], X[ratio:], Y[ratio:]
+
+    evaluationMLE = evaluation(Amounts[ratio:])
+    evaluationEM = evaluation(Amounts[ratio:])
+    evaluationEM2 = evaluation(Amounts[ratio:])
+
     customer = np.random.rand(y_train.shape[0], FLAGS.n_clusters)
     customer = customer / customer.sum(axis=1)[:, np.newaxis]
 
@@ -195,10 +207,6 @@ if __name__ == '__main__':
 
     x_train_nonfraud = x_train[np.argmax(y_train, axis=1) == 0, :]
     y_train_nonfraud = y_train[np.argmax(y_train, axis=1) == 0, :]
-
-
-    print x_train_nonfraud.shape, x_train_fraud.shape
-
 
     #p_c, x_given_c = fit(x_train_nonfraud[indexes], y_train_nonfraud[indexes], n_clusters=FLAGS.n_clusters, epochs=20, init='random')
 
@@ -209,28 +217,35 @@ if __name__ == '__main__':
 
     #x_given_f, p_f = np.load('pickles/p_x_given_f.npy'), np.load('pickles/p_f.npy')
 
+
     for n_samples in [1051, 1051 * 2, 1051 * 4, 1051 * 8, 1051 * 16, 1051 * 32]:
-        p_c = np.load('pickles/p_c(nonfraud)' + str(FLAGS.n_clusters) + "_" + str(n_samples) + '.npy')
-        x_given_c = np.load('pickles/x_given_c(nonfraud)' + str(FLAGS.n_clusters)+ "_" + str(n_samples) + '.npy')
+
+        p_c_nonfraud = np.load('pickles/p_c(nonfraud)' + str(FLAGS.n_clusters) + "_" + str(n_samples) + '.npy')
+        x_given_c_nonfraud = np.load('pickles/x_given_c(nonfraud)' + str(FLAGS.n_clusters)+ "_" + str(n_samples) + '.npy')
+
+        p_c_fraud = np.load('pickles/p_c(fraud)' + str(FLAGS.n_clusters) + '.npy')
+        x_given_c_fraud = np.load('pickles/x_given_c(fraud)' + str(FLAGS.n_clusters) + '.npy')
+
         indexes = np.random.choice(x_train_nonfraud.shape[0], n_samples)
 
         x_train_undersample = np.concatenate((x_train_nonfraud[indexes], x_train_fraud), axis=0)
         y_train_undersample = np.concatenate((y_train_nonfraud[indexes], y_train_fraud), axis=0)
 
+        #print "n_samples: ", n_samples
+        x_given_f, p_f = mle(x_train_undersample, y_train_undersample, 0)
 
-        #np.save('pickles/p_c(nonfraud)' + str(FLAGS.n_clusters) + "_" + str(n_samples), p_c)
-        #np.save('pickles/x_given_c(nonfraud)'+ str(FLAGS.n_clusters) + "_" + str(n_samples), x_given_c)
-
-        print "alpha: ", 0, "n_samples: ", n_samples
-        x_given_f, p_f = mle3(x_train_undersample, y_train_undersample, 0)
-
-        print "MLE: "
+        #print "MLE: "
         predict(x_given_f, p_f, x_test, y_test, n_samples)
 
-        print "MLE + EM: "
-        params = [p_c, x_given_c, x_given_f, p_f]
+        #print "MLE + EM: "
+        params = [p_c_nonfraud, x_given_c_nonfraud, x_given_f, p_f]
         predict_with_c(x_test, y_test, params, n_samples)
+
+        #print "EM + EM: "
+        params2 = [p_c_nonfraud, x_given_c_nonfraud, p_c_fraud, x_given_c_fraud, x_given_f, p_f]
+        predict_c1_c2(x_test, y_test, params2, n_samples)
 
     evaluationMLE.get_results().to_csv("results/" + "MLE_" + str(FLAGS.n_clusters), index=False)
     evaluationEM.get_results().to_csv("results/" + "EMandMLE_"+ str(FLAGS.n_clusters), index=False)
+    evaluationEM2.get_results().to_csv("results/" + "EMandEM_"+ str(FLAGS.n_clusters), index=False)
 
