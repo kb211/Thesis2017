21391a180852335d8de9516ff430e18563eedb53
Kasper Bouwens <kas_bouwens@hotmail.com>
2017-12-28 20:41:14 +0100

    working EM v.3

--- EM_v3.py
+++ EM_v3.py
@@ -6,66 +6,58 @@ class expectation_maximization:
         self.verbose = verbose
         self.thresh = thresh
 
-    def expectation(self, transactions, thetas):
+    def one_hot(self, array):
+        out = np.zeros((array.shape[0], np.max(array) + 1))
+        out[np.arange(array.shape[0]), array] = 1
+        return out
 
-        theta_T, theta_F, c_given_id = thetas[0], thetas[1], thetas[2]
-        y = np.asarray(np.zeros([transactions.shape[0], theta_F.shape[1]]))
+    def normalize(self, arr, axis=None):
+        arr = np.array(arr, copy=False, dtype=np.float)
+        assert arr.ndim > 0, 'This makes no sense on scalars'
+        if axis is None:
+            assert arr.ndim == 1, 'If array dim is not 1 you must specify axis'
+            axis = 0
+        assert np.all(arr >= 0)
+        return arr / np.sum(arr, axis=axis, keepdims=True)
 
-        log_likelihood, ll = 0, []
-        for t_value in range(theta_F.shape[1]):
-            p_vis_0 = self.bernouli(theta_F[:, t_value], transactions)
-            p_x_given_y = np.sum(p_vis_0, 1)
-            ll.append(p_x_given_y)
-            y[:, t_value] = np.log(theta_T[t_value]) + p_x_given_y
+    def expectation(self, f, x, ids, thetas):
+        x_given_c_f, c_given_q = thetas[0], thetas[1]
 
-        y = np.exp(y)/np.sum(np.exp(y), axis=1, keepdims=True)
-        for i in range(len(ll)):
-            log_likelihood += np.dot(y[:, i], ll[i])
-
-        return y, log_likelihood
-
-    def maximization(self, targets, transactions, ids, alpha=0.0):
-
-        theta_IDs = np.mean(ids, axis=0)
-
-        theta_C = np.mean(targets, axis=0)
-        C_given_ID = np.dot(targets.T, ids)
-
-        C_given_ID /= np.sum(C_given_ID, axis=0)
-
-        print C_given_ID
-        X_given_C = np.dot(targets.T,transactions) + alpha
-        X_given_C /= (np.sum(X_given_C, axis=1)[:,None] + alpha*2)
+        p_c = c_given_q[:, ids].T
 
 
-        return theta_C, X_given_C.T, C_given_ID
+        x_given_c = x_given_c_f[:, :, np.argmax(f, axis=1)].T
+        intermediate = (np.log(x_given_c) * x[:, None, :]) + (np.log(1 - x_given_c) * (1- x[:, None, :]))
 
-    def em_algorithm(self, t, f, ids, iterations):
+        p_x = np.log(p_c) + np.sum(intermediate, axis=2)
+        y = np.exp(p_x) / np.sum(np.exp(p_x), axis=1, keepdims=True)
+        return y
+
+    def maximization(self, f, x, ids, c, alpha=0.0):
+        c_given_q = (ids[:, :, None]* c[:, None, :]).sum(axis=0) / ids.sum(axis=0)[:, None]
+        x_given_c_f = ((f[:, :, None]* c[:, None, :])[:,:,:,None]*x[:, None, None, :]).sum(axis=0) / (f[:, :, None]* c[:, None, :]).sum(axis=0)[:, :, None]
+
+
+        return x_given_c_f.T, c_given_q.T
+
+    def em_algorithm(self, f, x, ids, iterations, n=3):
         values = []
         #I = (f != -1) * 1
 
-        theta_T, theta_F, c_given_id = self.maximization(t, f, ids, alpha=0.0001)
-
-        init_theta_T, init_theta_F = theta_T, theta_F
-
-        thresh = 0.0001
-        llikelihood_old = -np.infty
+        x_given_c_f = self.normalize(np.random.rand(x.shape[1], n, f.shape[1]), axis=0)
+        c_given_q = self.normalize(np.random.rand(n, np.unique(ids).shape[0]), axis=0)
+        p_q = self.normalize(np.bincount(ids))
+        p_f = np.mean(f, axis=0)
+        ids_oh = self.one_hot(ids)
 
         for i in range(iterations):
 
-            t, llikelihood_new = self.expectation(f, [theta_T, theta_F, c_given_id])
-            values.append(llikelihood_new)
+            thetas = [x_given_c_f, c_given_q]
+            c = self.expectation(f, x, ids, thetas)
 
-            theta_T, theta_F, c_given_id = self.maximization(t, f, ids, alpha=0.000001)
-            if self.verbose != 0:
-                print "Run %d produced theta of:" % i
-                print theta_T
+            x_given_c_f, c_given_q = self.maximization(f, x, ids_oh, c, alpha=0.000001)
 
-            if np.abs(llikelihood_new - llikelihood_old) < self.thresh and self.thresh !=0:
-                return theta_T, theta_F, values, [init_theta_T, init_theta_F]
-            llikelihood_old = llikelihood_new
-
-        return theta_T, theta_F, c_given_id
+        return x_given_c_f, c_given_q, p_q, p_f
 
     def bernouli(self, theta, x):
         result = np.zeros(x.shape)
